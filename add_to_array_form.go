cc8a6bcea28479f469878ab03c0e5f1ba3227662
Aleksandr Grigorev <cognize6@gmail.com>
2023-02-15 12:04:06 +0400

    Add improved version of addToArrayForm

--- add_to_array_form.go
+++ add_to_array_form.go
@@ -2,37 +2,16 @@
 
 package leetcode
 
-import (
-	"strconv"
-)
-
 func addToArrayForm(num []int, k int) []int {
-	sk := strconv.Itoa(k)
-	num2 := make([]int, len(sk))
-	for i, v := range sk {
-		num2[i] = int(v - '0')
+	ln := len(num)
+	for i := 0; i < ln && k > 0; i++ {
+		k += num[ln-i-1]
+		num[ln-i-1] = k % 10
+		k = k / 10
 	}
-	l1 := len(num)
-	l2 := len(num2)
-	l := max(l1, l2)
-	res := make([]int, l+1)
-	rem := 0
-	for i := 0; i < l; i++ {
-		ai := 0
-		bi := 0
-		if i < l1 {
-			ai = num[l1-i-1]
-		}
-		if i < l2 {
-			bi = num2[l2-i-1]
-		}
-		ri := ai + bi + rem
-		res[l-i] = ri % 10
-		rem = ri / 10
+	for k > 0 {
+		num = append([]int{k % 10}, num...)
+		k = k / 10
 	}
-	if rem != 0 {
-		res[0] = rem
-		return res
-	}
-	return res[1:]
+	return num
 }
